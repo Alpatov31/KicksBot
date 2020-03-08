6f4f21037e7c49fec842cbff38eb93b79e4ed7e6
Alpatov31 <alpatov31@yandex.ru>
2020-03-08 18:13:21 +0300

    Finish work with DataBase.

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import sqlite3
 import os
 
 
+def set_state(state, id):
+    sql = f'''UPDATE Users SET state='{state}' WHERE id={id} '''
+    cursor.execute(sql)
+    conn.commit()
+def set_property(name, value, id):
+    sql = f'''UPDATE Users SET {name}='{value}' WHERE id={id} '''
+    cursor.execute(sql)
+    conn.commit()
+def get_property(name, id):
+    sql = f"SELECT {name} FROM Users WHERE id={from_id} "
+    cursor.execute(sql)
+    return cursor.fetchone()[0]
+
+
+
+
 def process(intext):
     intext = intext.lower()
     if intext == "как дела?":
@@ -43,7 +59,7 @@ for i in imgs:
 
 colors = ["silver", "black", "gray", "white", "red"]
 sizes = ["8", "8_5", "9", "9_5", "10", "10_5", "11", "11_5"]
-users = {}
+
 # vk.method("messages.send", {"peer_id": 182479157, "message": "Bruh", "random_id": random.randint(1, 1000)})
 # messages = vk.method("messages.getConversations", value)
 # print(messages["items"][0]["last_message"]["text"])
@@ -54,16 +70,12 @@ while True:
         from_id = messages["items"][0]["last_message"]["from_id"]
         in_text = messages["items"][0]["last_message"]["text"]
 
-        if from_id not in users:
-            print(type(from_id))
-            users[from_id] = {"state": "start"}
-
         sql = f"SELECT * FROM Users WHERE id={from_id}"
         cursor.execute(sql)
         if len(cursor.fetchall()) == 0:
             sql = f'''
-            INSERT INTO Users
-            VALUES ({from_id}, 'start', 'red', '10')
+            INSERT INTO Users (id, state)
+            VALUES ({from_id}, 'start')
             '''
             cursor.execute(sql)
             conn.commit()
@@ -75,22 +87,22 @@ while True:
 
         if state == "start":
             out_text = "Привет, напиши свой размер(us)."
-            users[from_id]["state"] = "size"
+            set_state("size", from_id)
             kbrd = open("keyboards/keyboard.json", "r", encoding="UTF-8").read()
         elif state == "size":
             if in_text in sizes:
-                users[from_id]["size"] = in_text
+                set_property("size", in_text, from_id)
                 kbrd = open("keyboards/keybrd.json", "r", encoding="UTF-8").read()
                 out_text = "Отлично, теперь введи цвет кроссовок, которые ты хочешь по-английски."
-                users[from_id]["state"] = "color"
+                set_state("color", from_id)
             else:
                 out_text = "Введите один из: " + ", ".join(sizes)
         elif state == "color":
             if in_text.lower() in colors:
-                users[from_id]["color"] = in_text.lower()
-                out_text = "https://sneakerhead.ru/shoes/sneakers/" + users[from_id]["color"] + "/size-" + \
-                           users[from_id]["size"] + "/"
-                users[from_id]["state"] = "finish"
+                set_property("color", in_text.lower(), from_id)
+                out_text = "https://sneakerhead.ru/shoes/sneakers/" + get_property("color", from_id) + "/size-" + \
+                           get_property("size", from_id) + "/"
+                set_state("finish", from_id)
             else:
                 out_text = "Введите один из: " + ", ".join(colors)
 
