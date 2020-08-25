fc010e929e175470a238b1933809bec44e19d91e
Alpatov31 <61656974+Alpatov31@users.noreply.github.com>
2020-08-25 17:50:47 +0300

    Update db.py

--- db.py
+++ db.py
@@ -3,12 +3,13 @@ import sqlite3
 if __name__ == '__main__':
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-    # sql = '''
-    #     CREATE TABLE Users (
-    #     id integer PRIMARY KEY,
-    #     state text,
-    #     color integer,
-    #     size text);
-    # '''
-    cursor.execute('ALTER TABLE Users ADD COLUMN gender TEXT;')
+    sql = '''
+    CREATE TABLE Users (
+    id integer PRIMARY KEY,
+    state text,
+    color integer,
+    size text,
+    gender text);
+    '''
+    cursor.execute(sql)
     conn.commit()
